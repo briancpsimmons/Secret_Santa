43554a8a42a7cdd92009fcc738f8e6ee00c1d09b
Brian Simmons <briancpsimmons@gmail.com>
2021-03-12 15:41:18 -0500

    commiting the personMaker function

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
-//this is the list of the participating memebers of the family secret santas
+//factory function to create the list of participating members 
+function personMaker(name, family, role) {
+    const person = {
+        name,
+        family,
+        role,
+    }
+    return person
+};
 
-const simmonsSquad = ['Brian', 'Jackie', 'Quinn', 'Grace'];
-const hanFam = ['John', 'Cathie', 'Trish'];
-const kamClan = ['Pete', 'Michelle', 'Joanna'];
-const herronHill = ['Steve', 'Pennie', 'Michael', 'Lauren'];
-const southerners = ['JT', 'Katie', 'Nolan', 'Reese'];
+//make simmonsSquad
+personMaker('Brian', 'simmonsSquad', 'adult');
+personMaker('Jackie', 'simmonsSquad', 'adult');
+personMaker('Quinn', 'simmonsSquad', 'child');
+personMaker('Grace', 'simmonsSquad', 'child');
+//make kamClan
+personMaker('Pete', 'kamClan', 'adult');
+personMaker('Michelle', 'kamClan', 'adult');
+personMaker('Joanna', 'kamClan', 'child');
+//make hanFam
+personMaker('John', 'hanFam', 'adult');
+personMaker('Cathie', 'hanFam', 'adult');
+personMaker('Trish', 'hanFam', 'adult');
+//make herronHill
+personMaker('Steve', 'herronHill', 'adult');
+personMaker('Pennie', 'herronHill', 'adult');
+personMaker('Michael', 'herronHill', 'adult');
+personMaker('Lauren', 'herronHill', 'adult');
+//make southerners
+personMaker('JT', 'southerners', 'adult');
+personMaker('Katie', 'southerners', 'adult');
+personMaker('Nolan', 'southerners', 'child');
+personMaker('Reese', 'southerners', 'child');
 
 
+
+
+function assignToSimmonsFamily() {
+    const participatingFamilies = ['simmonsSquad', 'herronHill', 'southerners'];
+    const simmonsFam = [];
+    for (i = 0; i < person.length; i++) {
+        if (person.family === '')
+    }
+};
