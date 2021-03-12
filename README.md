e806326f4bcb2e1e914abb68413d3fa42b8d4edf
Brian Simmons <briancpsimmons@gmail.com>
2021-03-12 06:10:36 -0500

    inital commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Project: Secret Santa 
+
+## 1. Technologies & Requirements
+
+This was built using VS Code w/ git and node.js extensions.
+
+## 2. Project goals
+
+1. To test my newly developed skills learned from Codecademy.
+2. To replace the online Secret Santa application we use in December to match pairs.
+3. To impress my wife.
---
+++ index.js
@@ -0,0 +1,9 @@
+//this is the list of the participating memebers of the family secret santas
+
+const simmonsSquad = ['Brian', 'Jackie', 'Quinn', 'Grace'];
+const hanFam = ['John', 'Cathie', 'Trish'];
+const kamClan = ['Pete', 'Michelle', 'Joanna'];
+const herronHill = ['Steve', 'Pennie', 'Michael', 'Lauren'];
+const southerners = ['JT', 'Katie', 'Nolan', 'Reese'];
+
+
