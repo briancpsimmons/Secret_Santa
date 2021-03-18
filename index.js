381c46a0fb618c194dab737573a64114591622ff
Brian Simmons <briancpsimmons@gmail.com>
2021-03-18 15:17:31 -0400

    final push for initial build

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ function personMaker(name, family, role) {
 //make simmonsSquad
 personMaker('Brian', 'simmonsSquad', 'adult');
 personMaker('Jackie', 'simmonsSquad', 'adult');
-personMaker('Quinn', 'simmonsSquad', 'child');
-personMaker('Grace', 'simmonsSquad', 'child');
+//personMaker('Quinn', 'simmonsSquad', 'child');
+//personMaker('Grace', 'simmonsSquad', 'child');
 //make kamClan
 personMaker('Pete', 'kamClan', 'adult');
 personMaker('Michelle', 'kamClan', 'adult');
-personMaker('Joanna', 'kamClan', 'child');
+//personMaker('Joanna', 'kamClan', 'child');
 //make hanFam
 personMaker('John', 'hanFam', 'adult');
 personMaker('Cathie', 'hanFam', 'adult');
@@ -32,11 +32,13 @@ personMaker('Lauren', 'herronHill', 'adult');
 //make southerners
 personMaker('JT', 'southerners', 'adult');
 personMaker('Katie', 'southerners', 'adult');
-personMaker('Nolan', 'southerners', 'child');
-personMaker('Reese', 'southerners', 'child');
+//personMaker('Nolan', 'southerners', 'child');
+//personMaker('Reese', 'southerners', 'child');
 
-//function to assign people into Simmons Secret Santa
-const simmonsFamily = () => {
+//   <<<IDEALLY WOULD RE-WRITE THIS FUNCTION TO WORK ACROSS ANY INPUT FAMILY>>>
+
+//function to assign from people into the Simmons family Secret Santa
+const simmonsFamilyMembers = () => {
     const family = [];
     for (let i = 0; i < people.length; i++) {
         if (people[i].family === 'simmonsSquad' || people[i].family === 'herronHill' || people[i].family === 'southerners' ) {
@@ -45,9 +47,8 @@ const simmonsFamily = () => {
     }
     return family
 };
-
-//function to assign people into Hanrahan Secret Santa
-const hanrahanFamily = () => {
+//function to assign from people into the Hanrahan family Secret Santa
+const hanrahanFamilyMembers = () => {
     const family = [];
     for (let i = 0; i < people.length; i++) {
         if (people[i].family === 'simmonsSquad' || people[i].family === 'kamClan' || people[i].family === 'hanFam' ) {
@@ -57,19 +58,21 @@ const hanrahanFamily = () => {
     return family
 };
 
-//function to randomly assign Secret Santas to recipients across families
+//function to randomly assign recipients to Secret Santas
 const createPairs = arr => {
-//holding variable for my final matched pairs
+//holding variable for my final matched Santa/recipient pairs
     var pairs = {
         santa: [], 
         recipient: [],
     };
 //holding variable for family members already matched as receipients
     var usedFamilyMembers = [];
+//start looping through each member of the array
     for (let i = 0; i < arr.length; i++) {
+//holding variables for elligibile and inelligible family members for each person in the array
         var elligibleFamilyMembers = [];
         var inelligibleFamilyMembers = [];
-//this part creates the list of ellegibile family members for each member of the input array: SH made this work for me
+//creates the list of ellegibile family members for each member of the input array: SH made this work for me
         for (let j = 0; j < arr.length; j++) {
             if (arr[i].family !== arr[j].family && (usedFamilyMembers.indexOf(arr[j].name) === -1)) {
                 elligibleFamilyMembers.push(arr[j].name)
@@ -77,21 +80,16 @@ const createPairs = arr => {
                 inelligibleFamilyMembers.push(arr[j].name)
             }
         };
-//STUCK HERE!!!
-//need to loop through the elligibile family array and pick a random person that has not already been paired   
-        //do 
-        {
-            randomElligible = elligibleFamilyMembers[Math.floor(Math.random() * elligibleFamilyMembers.length)];
-            pairs.recipient.push(randomElligible);
-            usedFamilyMembers.push(randomElligible);
-        }
-        //while (usedFamily === randomElligible)
-//pushes santa values into the pairs object
+//randomly selects from the list of elligible family meembers for each person in the array and assigns them as recipient while also exlcluding them from future selection   
+    randomElligible = elligibleFamilyMembers[Math.floor(Math.random() * elligibleFamilyMembers.length)];
+    usedFamilyMembers.push(randomElligible);
+//pushes santa and recipient values into the pairs object
     pairs.santa.push(arr[i].name);
-    };
+    pairs.recipient.push(randomElligible);
+};
 //returns each santa + recipient matched pair
     return pairs
 };
 
-console.log(createPairs(simmonsFamily()));
-console.log(createPairs(hanrahanFamily()));
+console.log(createPairs(simmonsFamilyMembers()));
+console.log(createPairs(hanrahanFamilyMembers()));
